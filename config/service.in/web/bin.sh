b0dc1fb998c3686b43f70c187514465ccdf57941
Wakaba <wakabatan@hatena.ne.jp>
2012-10-30 20:05:25 +0900

    typo

--- config/service.in/web/bin.sh
+++ config/service.in/web/bin.sh
@@ -2,7 +2,7 @@
 exec 2>&1
 export KARASUMA_CONFIG_JSON=@@INSTANCECONFIG@@/@@INSTANCENAME@@.json
 export KARASUMA_CONFIG_FILE_DIR_NAME=@@LOCAL@@/keys
-export GHH_RULES_D=@@INSTANCECONFIG@@/config/rules
+export GHH_RULES_D=@@INSTANCECONFIG@@/rules
 
 export WEBUA_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl env.WEBUA_DEBUG text`
 port=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl ghh.web.port text`
