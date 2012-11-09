cc8c52f3c1c07bed7e7ee9743970692d1a4c4957
Wakaba <wakabatan@hatena.ne.jp>
2012-11-10 01:49:29 +0900

    debug

--- config/service.in/web/bin.sh
+++ config/service.in/web/bin.sh
@@ -5,6 +5,7 @@ export KARASUMA_CONFIG_FILE_DIR_NAME=@@LOCAL@@/keys
 export GHH_RULES_D=@@INSTANCECONFIG@@/rules
 
 export WEBUA_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl env.WEBUA_DEBUG text`
+export GHH_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl env.GHH_DEBUG text`
 port=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl ghh.web.port text`
 
 eval "exec setuidgid @@USER@@ @@ROOT@@/plackup $PLACK_COMMAND_LINE_ARGS \
